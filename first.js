e7be653696a4fc53206853cf35be8372c3cfe61e
Muhammad Uzair <riazuzair64@gmail.com>
2024-09-30 12:47:02 +0500

    ti-tac-game using javascript

---
+++ first.js
@@ -0,0 +1,10 @@
+// console.log("first javascript code is here");
+// console.log("i am starting javascript code today ");
+// console.log("i am starting javascript code now ");
+
+let x=25;
+let userNum = prompt("Guess the right number !");
+while(userNum != x){
+    userNum = prompt("you Guess wrong number , Try Again ");
+}
+console.log('Congrates');

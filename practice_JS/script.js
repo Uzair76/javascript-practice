65e148aac2fe553dd19c6d19ccb0af323e4e0bf2
Muhammad Uzair <riazuzair64@gmail.com>
2025-01-23 13:07:11 +0500

    practice variables and string leterals

---
+++ practice_JS/script.js
@@ -0,0 +1,35 @@
+/*
+let fname= prompt(" enter name");
+let str = "@" + fname + fname.length;
+console.log(str);
+*/
+
+
+
+/*
+const markHeight=1.69;
+const markMass=78;
+const johnHeight=1.76;
+const johnMass=92;
+
+const markBMI=markMass/markHeight**2;
+const johnBMI=johnMass/johnHeight**2;
+
+const markHasGreator= markBMI > johnBMI;
+
+console.log(markBMI,johnBMI,markHasGreator);
+*/
+
+//String literals 
+/*
+const firstName='John';
+const job='Teacher';
+const birthYear=1993;
+const year=2000;
+
+// const john= "I'm " + firstName +',a '+(year-birthYear)+' year  old '+job+'!';
+// console.log(john);
+const john= `I'm ${firstName} ,a ${year-birthYear} year old ${job} !`
+
+console.log(john);
+*/

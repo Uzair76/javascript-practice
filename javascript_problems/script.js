a7fd39acf3e91bd38221ac3996d3d5e341995683
Muhammad Uzair <riazuzair64@gmail.com>
2025-12-24 20:26:22 +0500

    done 1st three questions

--- javascript_problems/script.js
+++ javascript_problems/script.js
@@ -1,1 +1,18 @@
 console.log("Hello, JavaScript Problems!");
+
+let x = 10;
+console.log(x);
+
+// sum of two number 
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(2,8));
+
+// convert string to number
+let str = "56";
+console.log(typeof(str));
+let num = parseInt(str);
+console.log(typeof(num));
+
+// check even or odd 
